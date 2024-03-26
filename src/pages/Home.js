eaa90caf7f19afbc541c4a712cf4b9c0efeff36a
Barto <ponsbarto@gmail.com>
2024-03-26 16:55:56 +0100

    terminar diseño de home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BiTimeFive } from "react-icons/bi";
 
 const Home = () => {
   return (
@@ -82,6 +83,45 @@ const Home = () => {
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="servies d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/estrella1.png" alt="services" />
+                  <div>
+                    <h6>Envios gratis</h6>
+                    <p className="mb-0">Para pedidos de mas de 40€</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/estrella1.png" alt="services" />
+                  <div>
+                    <h6>Ofertas diarias</h6>
+                    <p className="mb-0">Ahorra en tu compra</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/estrella1.png" alt="services" />
+                  <div>
+                    <h6>Atencion al cliente 24/7</h6>
+                    <p className="mb-0">Cntacta con un experto</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/estrella1.png" alt="services" />
+                  <div>
+                    <h6>Pagos seguros</h6>
+                    <p className="mb-0">100% protegidos</p>
+                  </div>
+                </div>
+            
               </div>
             </div>
           </div>
