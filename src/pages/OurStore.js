d1121aa5bebc17d88de755d0761a894f380b7fa7
Barto <ponsbarto@gmail.com>
2024-03-27 14:55:00 +0100

    terminar estructura de organisacion de productos segun precio, letra, o antiguedad, ademas agragada la posiblidad de ver la cantidad de productos maxima por fila

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 
 const OurStore = () => {
+    const [grid, setGrid] = useState(4);
+    alert (grid);
   return (
     <>
       <Meta title={"Nuestra Tienda"} />
@@ -74,33 +76,94 @@ const OurStore = () => {
                   <h5 className="sub-title">Colores</h5>
                   <div>
                     <div>
-                        <ul className="colors ps-0">
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            
-                        </ul>
+                      <ul className="colors ps-0">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                      </ul>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid mb-4">
+                <div className="d-flex flex-wrap justify-content-between align-items-center">
+                  <div className="d-flex justify-content-between align-items-center gap-10">
+                    <p className="mb-0 d-block" style={{"width":"150px"}}>organizar por:</p>
+                    <select name="" className="form-control form-select" id="">
+                      <option value="manual">Destacados</option>
+                      <option value="best-selling" selected="selected">
+                        mejor valorados
+                      </option>
+                      <option value="title-ascending">A-Z</option>
+                      <option value="title-descending">Z-A</option>
+                      <option value="price-ascending">Precio ascendente</option>
+                      <option value="price-descending">
+                        Precio descendiente
+                      </option>
+                      <option value="created-ascending">
+                        de mas antiguo a mas nuevo
+                      </option>
+                      <option value="created-descending">
+                        de mas nuevo a mas antiguo
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalproducts mb-0">21 Productos</p>
+                    <div className="grid">
+                      <div className="d-flex gap-10 align-items-center">
+                        <img onClick={()=>{
+                            setGrid(4)
+                        }}
+                          src="images/gr4.svg"
+                          className="d-block img-fluid"
+                          alt="grid"
+                        />
+                        <img onClick={()=>{
+                            setGrid(3)
+                        }}
+                          src="images/gr3.svg"
+                          className="d-block img-fluid"
+                          alt="grid"
+                        />
+                        <img onClick={()=>{
+                            setGrid(2)
+                        }}
+                          src="images/gr2.svg"
+                          className="d-block img-fluid"
+                          alt="grid"
+                        />
+                        <img onClick={()=>{
+                            setGrid(1)
+                        }}
+                          src="images/gr.svg"
+                          className="d-block img-fluid"
+                          alt="grid"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
